95e0992c8ce5a18066a339c6638867b1fb8cde9a
Masaru26 <jonas.hirsch@gmx.at>
2019-04-02 20:34:48 +0200

    Ringbuffer completed

--- Hirsch_Ringbuffer/RingBuffer/Ringbuffer.cs
+++ Hirsch_Ringbuffer/RingBuffer/Ringbuffer.cs
@@ -6,10 +6,86 @@ using System.Threading.Tasks;
 
 namespace RingBuffer
 {
-    public class Ringbuffer
+    public class Ringbuffer<T>
     {
+        T[] innerBuffer;
 
+        #region Properties
+    
+        public int Length { get; set; }
 
+        private int _Last;
 
+        /// <summary>
+        /// Last = index des zueletzt eingefügten werts
+        /// </summary>
+        private int Newest
+        {
+            get
+            {
+                return _Last;
+            }
+            set
+            {
+                if(value >= Length)
+                {
+                    _Last = 0;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Ringbuffer(int Length)
+        {
+            this.Length = Length;
+            this.innerBuffer = new T[this.Length];
+
+            this.Newest = 0;
+        }
+
+        public Ringbuffer(IList<T> ic)
+        {
+            this.Length = ic.Count();
+            this.innerBuffer = new T[this.Length];
+
+            for (int i = 0; i < this.Length; i++)
+            {
+                Write(ic[i]);
+            }
+
+            this.Newest = 0;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Read()
+        {
+            Console.WriteLine(this.Newest + ": " + innerBuffer[this.Newest]);
+        }
+
+        public void Write(T value)
+        {
+            innerBuffer[this.Newest] = value;
+            this.Newest++;
+            Read();
+        }
+
+        public void Peek(int idx)
+        {
+            Console.WriteLine(idx + ": " + innerBuffer[idx]);
+        }
+
+        /*
+        public void Output()
+        {
+            throw new NotImplementedException();
+        }
+        */
+        #endregion
     }
 }
--- Hirsch_Ringbuffer/RingbufferConsole/Program.cs
+++ Hirsch_Ringbuffer/RingbufferConsole/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using RingBuffer;
 
 namespace RingbufferConsole
 {
     class Program
     {
+        static string PATH = "input.csv";
+        //static Random rnd = new Random();
+
         static void Main(string[] args)
         {
+            #region GACKS
+            //List<string> val = new List<string>();
+
+            //for (int i = 0; i < 500; i++)
+            //{
+            //    val.Add(i + ";" + rnd.Next(0,500) + ";");
+            //}
+
+            //File.WriteAllLines(PATH, val.ToArray());
+            //Console.WriteLine("DONE");
+            #endregion
+
+            using(StreamReader sr = new StreamReader(PATH))
+            {
+                string zeile = "";
+                while (!sr.EndOfStream)
+                {
+
+                }
+            }
+
+            Ringbuffer<int> rb = new Ringbuffer<int>();
+
+
         }
     }
 }
